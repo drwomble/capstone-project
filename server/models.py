59808e1ca01cf7333978bf196f16741b65bc13fe
Drew <drwomble27@gmail.com>
2023-07-09 22:21:24 -0600

    built user and deck models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,34 @@
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db
+from sqlalchemy.orm import validates    
+from sqlalchemy import MetaData
 
-# Models go here!
+class Deck(db.Model, SerializerMixin):
+    __tablename__ = 'decks'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String)
+    deck_name = db.Column(db.String)
+    price = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    
+    def __repr__(self):
+        return f'<Deck id:{self.id}, name:{self.deck_name}, price:{self.price}>'
+    
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    profile_picture = db.Column(db.String, default='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png')
+    bio = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    
+    def __repr__(self): 
+        return f'<User id:{self.id}, username:{self.username}, email:{self.email}'
+    
+    
