2f19c048f053aca6d7d2c44ed00af9540f2fdf85
Drew <drwomble27@gmail.com>
2023-07-10 14:26:52 -0600

    changed models

--- server/models.py
+++ server/models.py
@@ -12,12 +12,14 @@ class Deck(db.Model, SerializerMixin):
     brand = db.Column(db.String)
     deck_name = db.Column(db.String)
     price = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
-    users = db.relationship('User', back_populates='deck')
+    users = db.relationship('User', backref='deck')
     
-    serialize_only = ('id', 'brand', 'deck_name', 'price')
+    serialize_only = ('id', 'brand', 'deck_name', 'price', 'image')
     
     def __repr__(self):
         return f'<Deck id:{self.id}, name:{self.deck_name}, price:{self.price}>'
@@ -28,7 +30,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
     profile_picture = db.Column(db.String, default='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png')
     bio = db.Column(db.String, nullable=True)
     created_at = db.Column(db.DateTime, server_default = db.func.now())
@@ -36,6 +38,8 @@ class User(db.Model, SerializerMixin):
     
     decks = db.relationship('Deck', back_populates='user')
     spots = db.relationship('Spot', back_populates='user')
+    # user_decks = db.relationship('User_Deck', backref='user')
+    # user_spots = db.relationship('User_Spot', backref='user')
     
     serialize_only = ('id', 'username', 'email', '-password_hash', 'profile_picture', 'bio')
     
@@ -49,47 +53,49 @@ class Spot(db.Model, SerializerMixin):
     location = db.Column(db.String)
     image = db.Column(db.String)
     description = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
-    users = db.relationship('User', back_populates='spot')
+    # user_spots = db.relationship('User_Spot', backref='spot')
+    users = db.relationship('User', backref='spot')
     
     serialize_only = ('id', 'location', 'image', 'description')
     
     def __repr__(self):
         return f'<Spot id: {self.id}, location: {self.location}>'
     
-class User_Deck(db.Model, SerializerMixin):
-    __tablename__ = 'user_decks'
+# class User_Deck(db.Model, SerializerMixin):
+#     __tablename__ = 'user_decks'
     
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'))
-    wishlist = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, server_default = db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'))
+#     wishlist = db.Column(db.Boolean, default=False)
+#     created_at = db.Column(db.DateTime, server_default = db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
-    users = db.relationship('User', back_populates='user_deck', cascade='all')
-    decks = db.relationship('Deck', back_populates='user_deck', cascade='all')
+#     users = db.relationship('User', backref='user_deck', cascade='all')
+#     decks = db.relationship('Deck', backref='user_deck', cascade='all')
     
-    serialize_only = ('id', 'user_id', 'deck_id', 'wishlist')
+#     serialize_only = ('id', 'user_id', 'deck_id', 'wishlist')
     
-    def __repr__(self):
-        return f'<User deck id:{self.id}, user_id:{self.user_id}, deck_id:{self.deck_id}>'
+#     def __repr__(self):
+#         return f'<User deck id:{self.id}, user_id:{self.user_id}, deck_id:{self.deck_id}>'
     
-class User_Spot(db.Model, SerializerMixin):
-    __tablename__ = 'user_spots'
+# class User_Spot(db.Model, SerializerMixin):
+#     __tablename__ = 'user_spots'
     
-    id = db.Column(db.Integer, primary_key=True)
-    spots = db.Column(db.Integer, db.ForeignKey('spots.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    created_at = db.Column(db.DateTime, server_default = db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+#     id = db.Column(db.Integer, primary_key=True)
+#     spots = db.Column(db.Integer, db.ForeignKey('spots.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     created_at = db.Column(db.DateTime, server_default = db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
-    users = db.relationship('User', back_populates='user_spot', cascade='all')
-    spots = db.relationship('Spot', back_populates='user_spot', cascade='all')
+#     users = db.relationship('User', backref='user_spot', cascade='all')
+#     spots = db.relationship('Spot', backref='user_spot', cascade='all')
     
-    serialize_only = ('id', 'spots', 'user_id')
+#     serialize_only = ('id', 'spots', 'user_id')
     
-    def __repr__(self):
-        return f'User Spots id:{self.id}, user_spots:{self.user_spots}, user_id:{self.user_id}>'
+#     def __repr__(self):
+#         return f'User Spots id:{self.id}, user_spots:{self.user_spots}, user_id:{self.user_id}>'
--- server/seed.py
+++ server/seed.py
@@ -7,7 +7,7 @@ from random import randint, choice as rc
 
 # Local imports
 from app import app
-from models import db, Deck, Spot, User, User_Deck, User_Spot
+from models import * 
 from werkzeug.security import generate_password_hash
 
 if __name__ == '__main__':
@@ -16,9 +16,15 @@ if __name__ == '__main__':
         Deck.query.delete()
         User.query.delete()
         Spot.query.delete()
-        User_Deck.query.delete()
-        User_Spot.query.delete()
+        # User_Deck.query.delete()
+        # User_Spot.query.delete()
         print('Seeding data....')
         
+        deck_1 = Deck(brand='Toy Machine', deck_name='test', price=25, image='https://www.stokedboardshop.be/cdn/shop/products/Stoned-Skateboarding-Super-Stoned-Mario-Skateboard-Deck.jpg?v=1591725642')
+        deck_2 = Deck(brand='Girl', deck_name='test', price=45, image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817')
         
+        user_1 = User(username='test', email='test', password_hash=generate_password_hash('123', method='scrypt'), bio='test')
         
+        spot_1 = Spot(location= 'over there', image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817', description='its a spot')
+        
+        print('Done seeding data....')
