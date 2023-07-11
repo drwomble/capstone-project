bb43b6076a805a9f82c378d51ea2b30a0c83d716
Drew <drwomble27@gmail.com>
2023-07-11 17:56:41 -0600

    fetches for decks spots and check session

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  const [decks, setDecks] = useState([])
+  const [spots, setSpots] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch('/decks')
+    .then(r => r.json())
+    .then(data => setDecks(data))
+  }, [])
+
+  useEffect(() => {
+    fetch('/spots')
+    .then(r => r.json())
+    .then(data => setSpots(data))
+  }, [])
+
+  useEffect(() => {
+    fetch('/checksession').then(r => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user))
+      } else {
+        setUser(null)
+      }
+    });
+  }, []);
+
+
 }
+
 
 export default App;
--- server/seed.py
+++ server/seed.py
@@ -10,6 +10,7 @@ from app import app
 from models import Deck, User, Spot, User_Deck, User_Spot
 from werkzeug.security import generate_password_hash
 from config import db
+from flask import session
 
 if __name__ == '__main__':
     with app.app_context():
@@ -23,12 +24,17 @@ if __name__ == '__main__':
         
         deck_1 = Deck(brand='Toy Machine', deck_name='test', price=25, image='https://www.stokedboardshop.be/cdn/shop/products/Stoned-Skateboarding-Super-Stoned-Mario-Skateboard-Deck.jpg?v=1591725642')
         deck_2 = Deck(brand='Girl', deck_name='test', price=45, image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817')
+        deck_3 = Deck(brand='Girl', deck_name='test', price=45, image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817')
         
-        user_1 = User(username='test', email='test', password_hash=generate_password_hash('123', method='scrypt'), bio='test')
+        user_1 = User(username='test', email='test1', password_hash=generate_password_hash('123', method='scrypt'), bio='test')
+        user_2 = User(username='test', email='test2', password_hash=generate_password_hash('123', method='scrypt'), bio='test')
+        user_3 = User(username='test', email='test3', password_hash=generate_password_hash('123', method='scrypt'), bio='test')
         
         spot_1 = Spot(location='over there', image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817', description='its a spot')
+        spot_2 = Spot(location='over there', image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817', description='its a spot')
+        spot_3 = Spot(location='over there', image='https://mainlandskateandsurf.com/cdn/shop/products/girl-deck-bannerot-93-til-deck-teal_680x.png?v=1672354817', description='its a spot')
         
-        db.session.add_all([deck_1, deck_2, user_1, spot_1])
+        db.session.add_all([deck_1, deck_2, deck_3, user_1, user_2, user_3, spot_1, spot_2, spot_3])
         db.session.commit()
         
         print('Done seeding data....')
