1df41841cae809af7cea2b1462249c820f97f039
Drew <drwomble27@gmail.com>
2023-07-20 22:57:42 -0600

    homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
 
 const HomePage = () => {
 
     return (
         <div>
-
+            <div>
+                <h5>Skate Exchange</h5>
+                <img src="/Logo.png"/>
+            </div>
+            <h5>Get Started</h5>
+            <nav>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to='/signup'>Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to='/signin'>Sign In</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
